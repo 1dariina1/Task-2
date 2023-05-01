f71057c8ca26efb725199ae4aad3d156b6e032ff
Lenovo <Lenovo@DESKTOP-SUK2R0S>
2023-05-01 14:59:22 +0300

    Добавьте файлы проекта.

---
+++ Task 2/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.Design;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp5
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("How much money does Peter have: ");
+            int money = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter the cost of notebook:");
+            int notebook = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter the cost of pen:");
+            int pen = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter the number of notebook");
+            int notebookBuy = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter the cost of pen:");
+            int penBuy = Convert.ToInt32(Console.ReadLine());
+            int sum = notebook * notebookBuy + pen * penBuy;
+            Console.WriteLine($"Sum= {sum}");
+            if (money < sum)
+            {
+                Console.WriteLine("Not enough money!");
+            }
+            else
+            {
+                int r;
+                r = money - sum;
+                Console.WriteLine($"The rest of money:{r}");
+            }
+            Console.ReadLine();
+
+        }
+    }
+}
---
+++ Task 3/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task_3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter the number: ");
+            int num = Convert.ToInt32(Console.ReadLine());
+            int f = num / 100;//first digit
+            int s = (num / 10) % 10;//second digit
+            int t = num % 10;//third digit
+        
+            if (f >= s && f >= t && s>=t)
+            {
+                    Console.WriteLine("{0}{1}{2} ", f,s,t);
+            }
+            else if (f >= s && s <= t)
+            {
+                    Console.WriteLine("{0}{1}{2} ", f, t, s);
+            }
+            else if (t >= f && s >= f)
+            {
+     
+                    Console.WriteLine("{0}{1}{2} ", t, s, f);
+            }
+            else if (s >= f && t >= f)
+            {
+
+                Console.WriteLine("{0}{1}{2} ", s, t, f);
+            }
+            else if (s >= f && f >= t)
+            {
+
+                Console.WriteLine("{0}{1}{2} ", s, f, t);
+            }
+            Console.ReadLine();
+        }
+    }
+}
